1eb986f34ea2c2ed7c813dc115a1c590577e4863
Varun09871 <100066674+Varun09871@users.noreply.github.com>
2022-07-10 16:56:56 +0530

    Add files via upload

---
+++ jioplay.php
@@ -0,0 +1,248 @@
+<html>
+<head>
+<title> VARUN JIOTV</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<link rel="shortcut icon" type="image/x-icon" href="https://telegra.ph/file/a68db8a0d833fc23ffdba.jpg">
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-72Y5WWWNTR">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-72Y5WWWNTR');
+</script>
+
+<style>
+html {
+  font-family: Poppins;
+  background: #000;
+  margin: 0;
+  padding: 0
+}
+
+body{
+padding:0;
+margin:0;
+
+
+}
+.loading {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: #000;
+        z-index: 9999;
+    }
+    
+    .loading-text {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        margin: auto;
+        text-align: center;
+        width: 100%;
+        height: 100px;
+        line-height: 100px;
+    }
+    
+    .loading-text span {
+        display: inline-block;
+        margin: 0 5px;
+        color: green;
+        font-family: 'Quattrocento Sans', sans-serif;
+    }
+    
+    .loading-text span:nth-child(1) {
+        filter: blur(0px);
+        animation: blur-text 1.5s 0s infinite linear alternate;
+    }
+    
+    .loading-text span:nth-child(2) {
+        filter: blur(0px);
+        animation: blur-text 1.5s 0.2s infinite linear alternate;
+    }
+    
+    .loading-text span:nth-child(3) {
+        filter: blur(0px);
+        animation: blur-text 1.5s 0.4s infinite linear alternate;
+    }
+    
+    .loading-text span:nth-child(4) {
+        filter: blur(0px);
+        animation: blur-text 1.5s 0.6s infinite linear alternate;
+    }
+    
+    .loading-text span:nth-child(5) {
+        filter: blur(0px);
+        animation: blur-text 1.5s 0.8s infinite linear alternate;
+    }
+    
+    .loading-text span:nth-child(6) {
+        filter: blur(0px);
+        animation: blur-text 1.5s 1s infinite linear alternate;
+    }
+    
+    .loading-text span:nth-child(7) {
+        filter: blur(0px);
+        animation: blur-text 1.5s 1.2s infinite linear alternate;
+    }
+        .loading-text span:nth-child(8) {
+        filter: blur(0px);
+        animation: blur-text 1.5s 1.2s infinite linear alternate;
+    }
+        .loading-text span:nth-child(9) {
+        filter: blur(0px);
+        animation: blur-text 1.5s 1.2s infinite linear alternate;
+    }
+        .loading-text span:nth-child(10) {
+        filter: blur(0px);
+        animation: blur-text 1.5s 1.2s infinite linear alternate;
+    }
+
+    
+    @keyframes blur-text {
+        0% {
+            filter: blur(0px);
+        }
+        100% {
+            filter: blur(4px);
+        }
+    }
+
+    .plyr__video-wrapper::before {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        z-index: 10;
+        content: '';
+        height: 35px;
+        width: 35px;
+background: url('') no-repeat;
+        background-size: 35px auto, auto;
+    }
+
+    .plyr__video-wrapper::after {
+        position: absolute;
+        top: 15px;
+        left: 15px;
+        z-index: 10;
+        content: '';
+        height: 60px;
+        width: 60px;
+        background: url('https://telegra.ph/file/3ad408e47b56deb4b75c1.png') no-repeat;
+        background-size: 35px auto, auto;
+    }
+
+</style>
+<style>
+.logo-container {
+                position: absolute;
+                top: 5px;
+                left: 5px;
+                width: 60px;
+                height: 60px;
+            }
+
+            #logo {
+                position: fixed;
+                background-image: url("https://telegra.ph/file/3ad408e47b56deb4b75c1.png");
+                background-size: cover;
+                background-position: center;
+            }
+	  .plyr {
+                height: 100%;
+                width: 100%;
+            }
+.float {
+                height: 60px;
+                width: 60px;
+                z-index: 10;
+                border-radius: 50px;
+                box-shadow: 2px 2px 3px #999;
+            }
+.label-container{
+	            position: relative;
+                top: 5px;
+                left:70px;
+                display:table;
+                visibility: hidden;
+            }
+
+            .label-text{
+                color:#FFF;
+                background:rgba(51,51,51,0.5);
+                display:table-cell;
+                vertical-align:middle;
+                padding:10px;
+                border-radius:3px;
+            }
+
+            .label-arrow{
+                display:table-cell;
+                vertical-align:middle;
+                color:#333;
+                opacity:0.5;
+                transform: scaleX(-1);
+            }
+
+            a.float + div.label-container {
+              visibility: hidden;
+              opacity: 0;
+              transition: visibility 0s, opacity 0.5s ease;
+            }
+
+            a.float:hover + div.label-container{
+              visibility: visible;
+              opacity: 1;
+            }
+</style>
+
+<style>@media only screen and (orientation : landscape){video.rmp-video {object-fit:fill;}}</style>
+<script src="https://cdn.radiantmediatechs.com/rmp/7.0.3/js/rmp.min.js"></script>
+
+<script src="https://cdn.radiantmediatechs.com/rmp/5.10.2/js/rmp.min.js"></script>
+
+</head> 
+
+<body style="margin:0px; background:white;"> 
+<!--
+  <div id="loading" class="loading">
+<div class="loading-text">
+<span class="loading-text-words">V</span>
+<span class="loading-text-words">A</span>
+<span class="loading-text-words">R</span>
+<span class="loading-text-words">U</span>
+<span class="loading-text-words">N</span>
+<span class="loading-text-words">J</span>
+<span class="loading-text-words">I</span>
+<span class="loading-text-words">O</span>
+<span class="loading-text-words">T</span>
+<span class="loading-text-words">V</span>
+ 
+
+
+</div>
+</div>
+
+<div class="logo-container">
+ <a href="https://verified.telegram.ind.in/k114technology" target="_blank" rel="noopener noreferrer" class="float" id="logo"></a></div>
+-->
+        
+
+
+
+
+
+
+
+<div style="position: sticky;top: 0px;z-index:99;">
+
+
+<div  id="rmpPlayer"></div><br />
